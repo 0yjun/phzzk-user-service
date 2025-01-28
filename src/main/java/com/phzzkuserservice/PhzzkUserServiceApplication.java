65d6e648239ef6193ac65828b1c45ebc2b2b83ac
jun <an0yjun@naver.com>
2025-01-28 20:23:27 +0900

    first commit

---
+++ src/main/java/com/phzzkuserservice/PhzzkUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phzzkuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhzzkUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhzzkUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phzzkuserservice/PhzzkUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phzzkuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhzzkUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
